b712a069151c60f3ec6790820efbb9bd6793b1e3
xionghl <928089510@qq.com>
2018-02-10 14:31:45 +0800

    Initial commit

---
+++ js/query.js
@@ -0,0 +1,17 @@
+/**
+ * Created by xionghl on 2018/2/10.
+ */
+$(function(){
+   var queryParam = {
+       appId: 1,
+       lastAmount: null,
+       lastRareDegree: null,
+       pageNo: 1,
+       pageSize: 10,
+       petIds: [],
+       querySortType: "CREATETIME_ASC",
+       requestId: 1518243274602,
+       tpl: ""
+   }
+
+});
